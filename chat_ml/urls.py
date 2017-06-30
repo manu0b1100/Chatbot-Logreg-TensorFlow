5d943995dae73f3443ef7799b9dd927db421fe91
Manobhav Jain <manobhav.jain@gmail.com>
2017-07-01 01:11:13 +0530

    8

--- chat_ml/urls.py
+++ chat_ml/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'chatapp'
 
 urlpatterns = [
 url(r'^$', views.home, name='home'),
 url(r'tensorflow', views.tensorflow, name='tensorflow')
-        ]
+        ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

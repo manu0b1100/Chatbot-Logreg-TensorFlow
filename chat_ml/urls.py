6c9a1115415d756d6769c99bfec8f1def9e2632a
Manobhav Jain <manobhav.jain@gmail.com>
2017-06-30 18:12:59 +0530

    second

--- chat_ml/urls.py
+++ chat_ml/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 app_name = 'chatapp'
 
 urlpatterns = [
 url(r'^$', views.home, name='home'),
-url(r'getnword$', views.getnextword, name='getnword'),
 url(r'tensorflow', views.tensorflow, name='tensorflow')
         ]
--- chat_ml/views.py
+++ chat_ml/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render
 from django.shortcuts import render,HttpResponse,render_to_response
-from django.shortcuts import render, redirect
 from .Model.deeplearn import *
 from .Model.regression import *
-from .Model.wordpred import *
-from .models import *
-from sklearn.feature_extraction.text import CountVectorizer
+
 
 
 # Create your views here.
 
-def testfunc(request):
 
-
-    return HttpResponse("Aur bhai")
 
 def home(request):
     if request.is_ajax():
@@ -23,25 +16,6 @@ def home(request):
 
     return render(request,'chat_ml/chatengine.html',{'name':'manobhav'})
 
-
-def getnextword(request):
-    if request.is_ajax():
-        q=request.POST['query']
-        ans=getResultList(q)
-        return HttpResponse(ans)
-
-
-def getPred(request):
-    '''if request.is_ajax():
-        q=request.POST['query']
-        q=q.split()
-        p=""
-        if len(q)>0:
-            q=q[-1]
-            create_n_insert()
-            p=getTrieResults(q)
-        return HttpResponse(p)'''
-    pass
 
 def tensorflow(request):
     if request.is_ajax():

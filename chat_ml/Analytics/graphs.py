909832455b428cad4ab00677418a0da691756e08
Manobhav Jain <manobhav.jain@gmail.com>
2017-07-07 16:58:49 +0530

    data changed

--- chat_ml/Analytics/graphs.py
+++ chat_ml/Analytics/graphs.py
@@ -4,24 +4,24 @@ import dj_database_url
 from ChatBot.settings import *
 
 def initialise():
-    print(dj_database_url.config(conn_max_age=600))
+    # print(dj_database_url.config(conn_max_age=600))
+    #
+    # user = DATABASES['default']['USER']
+    # password = DATABASES['default']['PASSWORD']
+    # database_name = DATABASES['default']['NAME']
+    # port=DATABASES['default']['PORT']
+    # host=DATABASES['default']['HOST']
+    #
+    # database_url = 'postgresql://{user}:{password}@{host}:{port}/{database_name}'.format(
+    #     user=user,
+    #     password=password,
+    #     database_name=database_name,
+    #     host=host,
+    #     port=port
+    # )
+    # engine=sqlalchemy.create_engine(database_url)
 
-    user = DATABASES['default']['USER']
-    password = DATABASES['default']['PASSWORD']
-    database_name = DATABASES['default']['NAME']
-    port=DATABASES['default']['PORT']
-    host=DATABASES['default']['HOST']
-
-    database_url = 'postgresql://{user}:{password}@{host}:{port}/{database_name}'.format(
-        user=user,
-        password=password,
-        database_name=database_name,
-        host=host,
-        port=port
-    )
-    engine=sqlalchemy.create_engine(database_url)
-
-    # engine = sqlalchemy.create_engine('sqlite:////home/manobhav/PycharmProjects/ChatBot/db.sqlite3')
+    engine = sqlalchemy.create_engine('sqlite:////home/manobhav/PycharmProjects/ChatBot/db.sqlite3')
     frame=pd.read_sql_table('chat_ml_userquery', engine,parse_dates=['date'])
     frame.drop('date',axis=1,inplace=True)
     return frame.to_dict(orient="records")

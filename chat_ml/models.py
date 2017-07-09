81daa8d1f843c869329715faf207cd5a7b7cd54d
Manobhav Jain <manobhav.jain@gmail.com>
2017-07-09 22:42:17 +0530

    chal ja

--- chat_ml/models.py
+++ chat_ml/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class UserQuery(models.Model):
     query=models.CharField(max_length=1000)
     date=models.DateTimeField()
-    pclass=models.CharField(max_length=20,default='about')
+    pclass=models.CharField(max_length=255,default='about')
     session=models.CharField(max_length=10,default='0000')
     prob=models.FloatField(default=0.0)
     backend=models.CharField(max_length=10,default='logreg')
